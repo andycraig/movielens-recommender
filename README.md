f880eafaab1fc146df528da0f9260c2273584e21
Andrew <apc53c@gmail.com>
2017-10-03 08:45:41 +0100

    Now prepare.R handles missing movies.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # movielens-recommender
-Processes the MovieLens dataset for use in a recommender system
+
+Processes the MovieLens dataset for use in a recommender system, and provides functionality to make recommendations.
+
+### Preprocessing
+
+Do ratings matrix factorisation with:
+
+```
+cd code
+Rscript prepare.R config-1m.yaml
+```
+
+### Predicting
+
+To make a prediction:
+
+In progress
--- code/prepare.R
+++ code/prepare.R
@@ -1,16 +1,44 @@
 library(magrittr)
 
+args = commandArgs()
+
 # Load config.
-config = yaml::yaml.load_file('config-1m.yaml')
+config = yaml::yaml.load_file(args[[1]])
 
 # Load ratings data.
 ratings_df = readr::read_csv(config[["ratings_csv"]],
-                 col_names=c("UserID", "MovieID", "Rating", "Timestamp"))
+                             col_names=c("UserID", "MovieID", "Rating", "Timestamp"))
+n_movies = length(unique(ratings_df$MovieID)) 
+n_users = length(unique(ratings_df$UserID))
+# Check that there are no skipped UserIDs.
+assertthat::are_equal(n_users, max(ratings_df$UserID))
+# Not all MovieIDs are present.
+# So that there are not all-missing rows/columns in the ratings matrix, 
+# create a second ID for movies: MovieRow.
+if (n_movies < max(ratings_df$MovieID)) {
+  # Create equivalence between MovieID and MovieRow.
+  MovieID_and_row = unique(ratings_df$MovieID) %>%
+    tibble::tibble(MovieID = .,
+                   MovieRow = 1:length(.))
+  # Save this.
+  MovieID_and_row %>% readr::write_csv(config[["MovieID_and_row_file"]])
+  ratings_df = ratings_df %>% dplyr::left_join(MovieID_and_row)
+}
 
 # Convert to matrix.
 ratings_mat = ratings_df %$% 
-  Matrix::sparseMatrix(i = UserID, j = MovieID, x = Rating) %>% # i: row; j: col
+  Matrix::sparseMatrix(i = UserID, j = MovieRow, x = Rating) %>% # i: row; j: col
   as.matrix()
 
 # Factorise matrix.
-svd_object = softImpute::softImpute(ratings_mat, rank.max = 4)
+svd_object = softImpute::softImpute(ratings_mat, 
+                                    rank.max = config[["NMF"]][["n_components"]])
+
+# Check outputs are the expected size.
+assertthat::are_equal(n_users, nrow(svd_object$u))
+assertthat::are_equal(n_movies, nrow(svd_object$v))
+
+# Save output.
+write(svd_object$u, config[["factorised_users_file"]])
+write(svd_object$v, config[["factorised_movies_file"]])
+write(t(svd_object$d), config[["factorised_diag_file"]])
