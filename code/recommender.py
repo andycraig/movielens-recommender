c28ff9f06cff607a5ad1fbf763fb39eabc952fbf
Andrew <apc53c@gmail.com>
2017-10-04 08:54:32 +0100

    Updated rank-one update method in recommender.py to work for missing values.

--- code/recommender.py
+++ code/recommender.py
@@ -11,7 +11,7 @@ class Recommender:
 		"""
 		self.U = U
 		self.V = V
-		self.S = S # Vector. Matrix is np.diag(self.S).
+		self.diag_S = np.diag(S)
 		self.new_v = None
 		# Precompute U_transpose.
 		self.U_transpose = self.U.transpose()
@@ -23,22 +23,36 @@ class Recommender:
 		"""
 		# If haven't already done so for this new_v, do rank-one update.
 		if self.new_v == None or not (self.new_v == new_v).all():
-			self._rank_one_update(a=new_v)
-		# Dot product of new_v's projection into latent V space
+			self._rank_one_update(new_v)
+		# Dot product of new_v's projection into latent V space (which is last row of V_prime)
 		# with latent U space.
-		#TODO
+		predictions = self.U_prime * self.V_prime[-1, :]
 		return predictions
 
-	def _rank_one_update(self, a):
-		"""Using method from Brand 2006.
-		Update operation, from Table 1.
+	def _rank_one_update(self, c):
+		"""Using method from Brand 'Fast online SVD revisions for lightweight recommender systems',
+		Section B.1.
 
 			Args:
-				a: Column matrix to be appended.
+				a: Column matrix to be appended. Can have missing values.
 		"""
-		m = self.U_transpose * a
-		p_norm = np.linalg.norm(a - self.U * m)
-		K = [[np.diag(self.S), m], [0, p_norm]]
+		mask_nas = np.isnan(c)
+		missing_indices = np.where(mask_nas)
+		not_missing_indices = np.where(~mask_nas)
+		# Following notation in paper:
+		# c_closed (filled circle): vector of known values in c.
+		# c_open (open circle): vector of unknown values in c.
+		# U_closed, U_open: corresponding rows of U.
+		c_closed = c[not_missing_indices]
+		c_open = c[missing_indices]
+		U_closed = self.U[not_missing_indices, :]
+		U_open = self.U[missing_indices, :]
+		# Impute missing values.
+		#c_hat = (U_open * self.diag_S) * np.linalg.pinv(U_closed * self.diag_S) * c_closed
+		# Compute broken-arrow matrix (called K in Brand 2006, although not in this paper).
+		upper_right_block = self.diag_S * np.linalg.pinv(U_closed * self.diag_S) * c_closed
+		K = [[self.diag_S, upper_right_block],
+			 [0, np.linalg.norm(c_closed - U_closed * upper_right_block)]]
 		# Rediagonalise K to get U'^T K V' = S'.
-		self.U_prime, self.S_prime, self.V_prime  = np.linalg.svd(K)
-		# Now [X a] = U' S' V'^T = ([U P] U') S' ([V Q] V')^T
+		self.U_prime, _, self.V_prime  = np.linalg.svd(K)
+		# Now [X c] = U' S' V'^T = ([U P] U') S' ([V Q] V')^T
--- code/recommender_m1.py
+++ code/recommender_m1.py
@@ -5,9 +5,9 @@ import numpy as np
 # Load config.
 #TODO
 # Set up recommender.
-factorised_users = np.loadtxt(config['factorised_users_file']
-factorised_movies = np.loadtxt(config['factorised_movies_file']
-factorised_diag = np.loadtxt(config['factorised_diag_file']
+factorised_users = np.loadtxt(config['factorised_users_file'])
+factorised_movies = np.loadtxt(config['factorised_movies_file'])
+factorised_diag = np.loadtxt(config['factorised_diag_file'])
 recommender = Recommender(U = factorised_movies,
 							V = factorised_users,
 							S = factorised_diag)
