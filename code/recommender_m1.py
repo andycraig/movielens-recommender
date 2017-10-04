cbc0a2151494cb60c739994c73ec0da17eb02239
Andrew <apc53c@gmail.com>
2017-10-04 09:03:14 +0100

    Made MovieLensRecommender class.

--- code/recommender_m1.py
+++ code/recommender_m1.py
@@ -1,18 +1,21 @@
 from recommender import Recommender
 import numpy as np
+import pandas as pd
 
-# Setup.
-# Load config.
-#TODO
-# Set up recommender.
-factorised_users = np.loadtxt(config['factorised_users_file'])
-factorised_movies = np.loadtxt(config['factorised_movies_file'])
-factorised_diag = np.loadtxt(config['factorised_diag_file'])
-recommender = Recommender(U = factorised_movies,
-							V = factorised_users,
-							S = factorised_diag)
+class MovieLensRecommender:
 
-# Make a prediction.
-#TODO new_user_ratings =
-predictions = recommender.predict_for_new_v(new_user_ratings)
-#TODO return or print.
+	def __init__(self, config):
+		factorised_users = np.loadtxt(config['factorised_users_file'])
+		factorised_movies = np.loadtxt(config['factorised_movies_file'])
+		factorised_diag = np.loadtxt(config['factorised_diag_file'])
+		self.MovieID_df = pd.read_csv(config['MovieID_and_row_file'])
+		self.recommender = Recommender(U = factorised_movies,
+									V = factorised_users,
+									S = factorised_diag)
+
+	def get_predictions(new_user_ratings):
+		# Make a prediction.
+		predictions = self.recommender.predict_for_new_v(new_user_ratings)
+		# predictions are by MovieRow, so need to convert to MovieID.
+		self.MovieID_df = self.MovieID_df.assign(prediction = predictions)
+		#TODO return 
