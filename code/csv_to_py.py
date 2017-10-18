c40dcd47523ff1790a782e38869b5d12fb58666a
Andrew <apc53c@gmail.com>
2017-10-18 17:57:07 +0100

    Partway through switching to use .py data files.

--- code/csv_to_py.py
+++ code/csv_to_py.py
@@ -1,8 +1,23 @@
+# Copy the contents of text and CSV files into .py files.
+
 import numpy as np
 import pandas as pd
+import yaml, sys
 
-#TODO Save the contents of these files into .py files.
-np.loadtxt(config['factorised_users_file'])
-np.loadtxt(config['factorised_movies_file'])
-np.loadtxt(config['factorised_diag_file'])
-pd.read_csv(config['MovieID_and_row_file'])
+def txt_to_py(source, dest):
+	print("Loading from " + source)
+	with open(source, 'r') as f:
+		lines = f.readlines()
+	with open(dest, 'w') as f:
+		f.write('x = "')
+		for line in lines:
+			f.write(line)
+		f.write('"')
+	print("Wrote to " + dest)
+
+with open(sys.argv[1]) as f:
+	config = yaml.load(f)
+txt_to_py(config['factorised_users_file'], config['factorised_users_py'])
+txt_to_py(config['factorised_movies_file'], config['factorised_movies_py'])
+txt_to_py(config['factorised_diag_file'], config['factorised_diag_py'])
+txt_to_py(config['MovieID_and_row_file'], config['MovieID_and_row_py'])
--- code/demo.py
+++ code/demo.py
@@ -1,23 +1,21 @@
 from recommender_m1 import MovieLensRecommender
 import pandas as pd
+import numpy as np
 import yaml
+from data import factorised_movies, factorised_users, factorised_diag, MovieID_and_row
 
 with open("config-1m.yaml") as f:
     config = yaml.load(f)
 
-factorised_users = np.loadtxt(config['factorised_users_file'])
-factorised_movies = np.loadtxt(config['factorised_movies_file'])
-factorised_diag = np.loadtxt(config['factorised_diag_file'])
-movie_df = pd.read_csv(config['MovieID_and_row_file'])
-
-m = MovieLensRecommender(factorised_users=factorised_users,
-                         factorised_movies=factorised_movies,
-                         factorised_diag=factorised_diag,
-                         movie_df=movie_df)
+#TODO Preprocess factorised_users.
+m = MovieLensRecommender(factorised_users=factorised_users),
+                         factorised_movies=np.loadtxt(config['factorised_movies_file']),
+                         factorised_diag=np.loadtxt(config['factorised_diag_file']),
+                         movie_df=pd.read_csv(config['MovieID_and_row_file']))
 movies_df = pd.read_csv(config['movies_csv'])
 n_movies = len(movies_df)
 
-# Some random ratings. See if we can get it to recommend action movies.
+# Some ratings for an action movie fan.
 new_user_ratings = {'Die Hard':5, 'Terminator':5, 'Speed':5}
 print("Ratings for new user:")
 print(new_user_ratings)
@@ -28,3 +26,5 @@ print("Worst movies for this user:")
 print(predictions_df.sort_values('prediction', ascending=True).head())
 print("Top predictions at or before 1960 for this user:")
 print(predictions_df.loc[predictions_df['Year'] <= 1960, :].sort_values('prediction', ascending=False).head())
+print("Predictions for movies that were actually rated for this user:")
+print(predictions_df.loc[predictions_df['Title'].isin(new_user_ratings), :])
