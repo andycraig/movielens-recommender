fae603233fb2cd1937c0726337abf71466331121
Andrew Craig <apc53c@gmail.com>
2017-12-24 18:59:47 +0000

    Fixed issue whereby there were NAs in the movie file.

--- code/csv_to_py.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-#TODO Save the contents of these files into .py files.
-np.loadtxt(config['factorised_users_file'])
-np.loadtxt(config['factorised_movies_file'])
-np.loadtxt(config['factorised_diag_file'])
-pd.read_csv(config['MovieID_and_row_file'])
